cc7b7003aab98a553a74084bb7e2706271ea625d
Yashraj Patil <yashrajpatil2206@gmail.com>
2025-10-04 12:15:29 +0530

    imports issue resolved

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Text, Numeric, Boolean
 from sqlalchemy.sql import func
 from .database import Base
 
